74ccd1cf59173f7b6ad1f6b3eca044bd0bf20150
Keenan Mueller <keenanmueller@gmail.comgit config --global user.email keenanmueller@gmail.comgit config --global user.email keenanmueller@gmail.com>
2017-11-16 21:52:00 +0000

    It's all working

--- comment/public/javascripts/app.js
+++ comment/public/javascripts/app.js
@@ -12,10 +12,18 @@ function controller($scope,$http){
     });
     $scope.formContent = '';
   };
+  
+  $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+        });
+    };
 
   $scope.incrementUpvotes = function(comment) {
-    comment.upvotes += 1;
-  };
+      $scope.upvote(comment);
+    };
 
   $scope.getAll = function() {
     return $http.get('/comments').success(function(data){
