0124f86c6302a378e4e9258261e9dfb320b45374
Kayla Bixler <kaylabix@comcast.net>
2017-11-15 22:04:12 -0700

    javascripts/app.js updated

--- comment/public/javascripts/app.js
+++ comment/public/javascripts/app.js
@@ -1,31 +1,33 @@
 
 angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
- $scope.addComment = function() {
-      if($scope.formContent === '') { return; }
-      console.log("In addComment with "+$scope.formContent);
-      $scope.create({
-        title: $scope.formContent,
-        upvotes: 0,
-      });
-      $scope.formContent = '';
-    };
- $scope.incrementUpvotes = function(comment) {
-      comment.upvotes += 1;
-    };
+  .controller('MainCtrl', ['$scope','$http', controller]);
+
+function controller($scope,$http){
+  $scope.addComment = function() {
+    if($scope.formContent === '') { return; }
+    console.log("In addComment with "+$scope.formContent);
+    $scope.create({
+      title: $scope.formContent,
+      upvotes: 0,
+    });
+    $scope.formContent = '';
+  };
+
+  $scope.incrementUpvotes = function(comment) {
+    comment.upvotes += 1;
+  };
+
   $scope.getAll = function() {
     return $http.get('/comments').success(function(data){
       angular.copy(data, $scope.comments);
     });
   };
+
   $scope.getAll();
 
   $scope.create = function(comment) {
     return $http.post('/comments', comment).success(function(data){
       $scope.comments.push(data);
     });
-  };
-
-]);
+  }
+}
